7bfdc555c9bb55585d8c79a92ee502c8ab8b1ed5
Gagandeep Singh Shekhawat <160869099+Shekhawat12082003@users.noreply.github.com>
2025-05-17 11:25:10 +0530

    hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,20 +11,19 @@ module.exports = {
    defaultNetwork: 'testnet',
 
    networks: {
-      hardhat: {
-      },
+      hardhat: {},
       testnet: {
-         url: 'https://rpc.test.btcs.network',
-         accounts: ['e0a951788665661e6e049e6adbf8d732de612898e5cba335cc64bd27ad8c661d'],
-         chainId: 1115,  // Ensure this matches the Core testnet chain ID
+         url: 'https://rpc.test2.btcs.network',
+         accounts: ['e0a951788665661e6e049e6adbf8d732de612898e5cba335cc64bd27ad8c661d'], 
+         chainId: 1114, 
       }
    },
    solidity: {
       compilers: [
         {
-           version: '0.8.21',  // Update to at least 0.8.20 to support the 'paris' EVM
+           version: '0.8.24', 
            settings: {
-              evmVersion: 'paris',  // Specify 'paris' EVM version
+              evmVersion: 'shanghai', 
               optimizer: {
                  enabled: true,
                  runs: 200,
@@ -39,6 +38,6 @@ module.exports = {
       artifacts: './artifacts',
    },
    mocha: {
-      timeout: 20000,  // You can adjust the timeout as needed
+      timeout: 20000,
    },
 };
